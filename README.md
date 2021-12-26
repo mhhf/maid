f51fe7124469da3900b3ba86ac421950916a5fac
Denis <denis.erfurt@gmail.com>
2021-12-26 18:13:27 +0100

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+Maid - helps keep my pc clean
+
+## Usage
+
+### fix
+```
+maid fix
+```
+
+goes through every directory in `~/src` and ensures everything is upstream and checked in
---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  description = "A very basic bash cli flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: 
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        packages = with pkgs; [
+          coreutils
+          makeWrapper
+          bash
+          # your packages here
+          dhall
+          dhall-bash
+          git
+          tree
+        ]; 
+      in
+      {
+        packages.maid = pkgs.stdenv.mkDerivation rec {
+          version = "0.1.0";
+          name = "maid-${version}";
+          src = ./.;
+          dontBuild = true;
+          doCheck = true;
+          checkTarget = "test";
+          makeFlags = ["prefix=$(out)"];
+          postInstall =
+            let
+              path = pkgs.lib.makeBinPath packages;
+            in
+              ''
+                wrapProgram "$out/bin/maid" \
+                  --prefix PATH : ${path}
+          '';
+          buildInputs = packages;
+          # shellHook = ''
+          # '';
+        };
+
+        defaultPackage = self.packages.x86_64-linux.maid;
+
+        devShell = pkgs.mkShell {
+          buildInputs = packages;
+        };
+    });
+}
